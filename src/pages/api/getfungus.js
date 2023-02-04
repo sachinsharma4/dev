d166bf19300693fdfe3c991a013936f7ab9e0e82
sachin <sachinshaarma7673@gmail.com>
2023-02-04 11:08:14 +0530

    commit

--- src/pages/api/getfungus.js
+++ src/pages/api/getfungus.js
@@ -2,10 +2,11 @@ import { Schema, model, connect } from 'mongoose';
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import Fungi from 'fungi/fungischema';
 import connectdb from 'fungi/middelware';
-
+mongoose.set('strictQuery', false);
 const handler = async (req,res) => {
    
    await connectdb();
+   
    
     res.status(200).json(await Fungi.find(req.query).catch((err)=> res.status(400).json({ err }))
     )
