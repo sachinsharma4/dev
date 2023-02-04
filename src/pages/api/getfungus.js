77430bd33b79065ff016065886c46586920d6fd9
sachinsharma4 <66316872+sachinsharma4@users.noreply.github.com>
2023-02-04 10:46:36 +0530

    Add files via upload

---
+++ src/pages/api/getfungus.js
@@ -0,0 +1,15 @@
+import { Schema, model, connect } from 'mongoose';
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import Fungi from 'fungi/fungischema';
+import connectdb from 'fungi/middelware';
+
+const handler = async (req,res) => {
+   
+   await connectdb();
+   
+    res.status(200).json(await Fungi.find(req.query).catch((err)=> res.status(400).json({ err }))
+    )
+    console.log(req)
+    console.log(res)
+}
+export default handler
