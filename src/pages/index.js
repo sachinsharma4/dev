1c3e0567503968e82d2e0b76da7a0b744c35ef30
sachin <sachinshaarma7673@gmail.com>
2023-02-04 14:28:24 +0530

    added voice search

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,10 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
-import logo from '../../public/logo.png'
-
 import { BiMicrophone, BiCamera } from 'react-icons/bi';
-
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState} from 'react'
 //import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-import { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+
 const inter = Inter({ subsets: ['latin'] })
 
 
@@ -26,8 +23,14 @@ const inter = Inter({ subsets: ['latin'] })
       const [Fungi,setFungi]=useState(props.data);
       const [placeholder,setplaceholder]=useState("enter the name fungi");
 
+    const record = ()=>{ setplaceholder("listening");
+    var recognition = new webkitSpeechRecognition();
+    recognition.lang = "en-US";
+    recognition.onresult = function(event) {
+      setvalue(event.results[0][0].transcript);}
+    recognition.onerror = function(error) {
+      setvalue("Enter the name of fungi");}}
     
-          
        
          
  
@@ -49,7 +52,7 @@ const inter = Inter({ subsets: ['latin'] })
           <span className=''>A Database of Fungi by Kirori Mal College</span>
           {/*created a search box todo implement image and voice search */}
           <form action={`https://dev-puce.vercel.app/fungi/${value}`} method="post" className='flex justify-center'>
-            <input value={value} list="fungi" onChange={(e) => setvalue(e.target.value)} placeholder={placeholder} className='rounded-full w-8/12 p-2 mb-1 text-black outline-none' />{/*<BiMicrophone  className='text-blue-900  -translate-x-14 my-3'/><BiCamera className='text-blue-900  -translate-x-12 my-3'/>*/}</form>
+            <input value={value} list="fungi" onChange={(e) => setvalue(e.target.value)} placeholder={placeholder} className='rounded-full w-8/12 p-2 mb-1 text-black outline-none' /><BiMicrophone onClick={record}  className='text-blue-900  -translate-x-14 my-3'/>{/*<BiCamera className='text-blue-900  -translate-x-12 my-3'/>*/}</form>
           <datalist id='fungi'>
             {
               Fungi.map((item) => {
